6d42a3a971dd90ea017b6e0fbf4795cf853ce9ac
Jasraj <jsb78241@gmail.com>
2025-08-09 13:46:19 -0400

    Update estimate-e.cpp

--- c-cpp-stuff/small-files/estimate-e.cpp
+++ c-cpp-stuff/small-files/estimate-e.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int factorial (int a) {
-if (a==0) return 1;
-int ans =1;
-for (int i = 1; i <= a; i++){
+long double factorial (int a) {
+if (a==0) return 1.0L;
+long double  ans =1.0L;
+for (long i = 1; i <= a; i++){
 ans *= i;
 }
 return ans;
@@ -12,9 +12,10 @@ return ans;
 int main() {
 printf("n= ");
 int n; cin >> n; 
-double e = 0;
-e+=(1.0/(double)factorial(n));
-while (n--) e+=(1.00/(double)factorial(n));
+long double e = 0.0L;
+for (int i = 0; i <= n; i++) {
+e+=(1.0L/factorial(i));
+}
 cout << e << '\n';
 return 0;
 }
