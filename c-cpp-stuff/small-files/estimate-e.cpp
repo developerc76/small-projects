02b3ba71e5833b87e520618447b68b911a26875e
Jasraj <jsb78241@gmail.com>
2025-08-09 12:26:39 -0400

    Create estimate-e.cpp

---
+++ c-cpp-stuff/small-files/estimate-e.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int factorial (int a) {
+if (a==0) return 1;
+int ans =1;
+for (int i = 1; i <= a; i++){
+ans *= i;
+}
+return ans;
+}
+int main() {
+printf("n= ");
+int n; cin >> n; 
+int e = 1;
+while (n--) e*=(1/factorial(n));
+cout << e << '\n';
+return 0;
+}
